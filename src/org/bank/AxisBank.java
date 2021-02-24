bffde7de8cad3ab328937ad08f2b6f5a35ff45d4
shivamani455 <shivamani455@gmail.com>
2021-02-24 10:09:41 +0530

    my test

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,11 @@
+package org.bank;
+public class AxisBank extends Bankinfo {
+public void deposit() {
+	
+System.out.println("9%");
+}
+public static void main(String[] args) {
+	AxisBank op=new AxisBank();
+	op.deposit();
+}
+}
---
+++ src/org/bank/Bankinfo.java
@@ -0,0 +1,13 @@
+package org.bank;
+
+public class Bankinfo {
+public void saving() {
+	System.out.println("6%");
+}
+public void fixed() {
+System.out.println("8%");	
+}
+public void deposit() {
+System.out.println("7%");
+}
+}
